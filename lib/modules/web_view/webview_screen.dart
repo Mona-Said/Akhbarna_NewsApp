ea85b2f9a95513e4115775dfe0db4bdaa23d6d7b
Mona Said <mona.said162002@gmail.com>
2024-11-12 03:03:47 +0200

    updated gradle & webview

--- lib/modules/web_view/webview_screen.dart
+++ lib/modules/web_view/webview_screen.dart
@@ -1,18 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewScreen extends StatelessWidget {
+class WebViewScreen extends StatefulWidget {
   final String? url;
 
-   const WebViewScreen({super.key,  this.url});
+  const WebViewScreen({super.key, this.url});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create and initialize the WebViewController
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(
+          Uri.parse(widget.url ?? 'https://flutter.dev')); // Use widget.url
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: WebView(
-          initialUrl: url,
-        ),
+    return SafeArea(
+      child: WebViewWidget(
+        controller: controller,
+      ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+//
+// class WebViewScreen extends StatelessWidget {
+//   final String? url;
+//
+//   const WebViewScreen({super.key, this.url});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//       body: Webview(
+//         url: url,
+//       ),
+//     );
+//   }
+// }
